7d8414d8ee884339154434a34633ca14314cc9d9
yuji5296 <yuji5296@be446a46-2433-11df-8743-957545a88a67>
2011-01-17 15:20:19 +0000

    2011/01/11 Ver.1.1.0 ・スプラッシュ画面、アプリ情報画面のレイアウト変更

--- trunk/Android/KivaJapanReader/gen/jp/kivajapan/rssreader/R.java
+++ trunk/Android/KivaJapanReader/gen/jp/kivajapan/rssreader/R.java
@@ -8,41 +8,51 @@
 package jp.kivajapan.rssreader;
 
 public final class R {
+    public static final class anim {
+        public static final int progresscircle=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class color {
-        public static final int backgroundcolor=0x7f050002;
-        public static final int list_row_background_normal=0x7f050004;
-        public static final int textcolor=0x7f050003;
-        public static final int titlebackgroundcolor=0x7f050000;
-        public static final int titletextcolor=0x7f050001;
+        public static final int backgroundcolor=0x7f060002;
+        public static final int list_row_background_normal=0x7f060004;
+        public static final int textcolor=0x7f060003;
+        public static final int titlebackgroundcolor=0x7f060000;
+        public static final int titletextcolor=0x7f060001;
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
         public static final int jpg674364=0x7f020001;
         public static final int kiva_icon=0x7f020002;
         public static final int kiva_japan=0x7f020003;
+        public static final int progresscircle_background=0x7f020004;
+        public static final int progresscircle_small=0x7f020005;
     }
     public static final class id {
-        public static final int AppName=0x7f090002;
-        public static final int Copyright=0x7f090005;
-        public static final int ImageView01=0x7f090001;
-        public static final int LinearLayout01=0x7f09000c;
-        public static final int TextView04=0x7f090000;
-        public static final int Version=0x7f090003;
-        public static final int WebView01=0x7f090009;
-        public static final int WebView02=0x7f090006;
-        public static final int item_author=0x7f09000e;
-        public static final int item_container=0x7f09000a;
-        public static final int item_descr=0x7f09000f;
-        public static final int item_detail_author=0x7f090008;
-        public static final int item_detail_title=0x7f090007;
-        public static final int item_image=0x7f09000b;
-        public static final int item_title=0x7f09000d;
-        public static final int link01=0x7f090004;
-        public static final int menu01=0x7f090010;
-        public static final int menu02=0x7f090011;
-        public static final int menu03=0x7f090012;
+        public static final int AppName=0x7f0a0004;
+        public static final int Copyright=0x7f0a0009;
+        public static final int ImageView01=0x7f0a0003;
+        public static final int LinearLayout01=0x7f0a0002;
+        public static final int LinearLayout02=0x7f0a0013;
+        public static final int ProgressBar01=0x7f0a0014;
+        public static final int ScrollView01=0x7f0a0001;
+        public static final int TextView01=0x7f0a0006;
+        public static final int TextView02=0x7f0a0008;
+        public static final int TextView04=0x7f0a0000;
+        public static final int Version=0x7f0a0005;
+        public static final int WebView01=0x7f0a000d;
+        public static final int WebView02=0x7f0a000a;
+        public static final int item_author=0x7f0a0011;
+        public static final int item_container=0x7f0a000e;
+        public static final int item_descr=0x7f0a0012;
+        public static final int item_detail_author=0x7f0a000c;
+        public static final int item_detail_title=0x7f0a000b;
+        public static final int item_image=0x7f0a000f;
+        public static final int item_title=0x7f0a0010;
+        public static final int link01=0x7f0a0007;
+        public static final int menu01=0x7f0a0015;
+        public static final int menu02=0x7f0a0016;
+        public static final int menu03=0x7f0a0017;
     }
     public static final class layout {
         public static final int about=0x7f030000;
@@ -53,31 +63,32 @@ public final class R {
         public static final int splash=0x7f030005;
     }
     public static final class menu {
-        public static final int menu=0x7f080000;
+        public static final int menu=0x7f090000;
     }
     public static final class string {
-        public static final int app_name=0x7f060002;
-        public static final int author=0x7f060004;
-        public static final int hello=0x7f060001;
-        public static final int help=0x7f06000a;
-        public static final int info=0x7f06000b;
-        public static final int loding=0x7f06000d;
-        public static final int mail=0x7f060005;
-        public static final int test=0x7f060000;
-        public static final int update=0x7f06000c;
-        public static final int url_code=0x7f060007;
-        public static final int url_kiva=0x7f060008;
-        public static final int url_kivajapan=0x7f060009;
-        public static final int url_site=0x7f060006;
-        public static final int version=0x7f060003;
+        public static final int app_name=0x7f070002;
+        public static final int author=0x7f070004;
+        public static final int hello=0x7f070001;
+        public static final int help=0x7f07000a;
+        public static final int info=0x7f07000b;
+        public static final int loding=0x7f07000d;
+        public static final int mail=0x7f070005;
+        public static final int test=0x7f070000;
+        public static final int update=0x7f07000c;
+        public static final int url_code=0x7f070007;
+        public static final int url_kiva=0x7f070008;
+        public static final int url_kivajapan=0x7f070009;
+        public static final int url_site=0x7f070006;
+        public static final int version=0x7f070003;
     }
     public static final class style {
-        public static final int KivaJapanStyle=0x7f070000;
-        public static final int WindowTitle=0x7f070002;
-        public static final int WindowTitleBackground=0x7f070001;
+        public static final int KivaJapanStyle=0x7f080000;
+        public static final int WindowTitle=0x7f080002;
+        public static final int WindowTitleBackground=0x7f080001;
     }
     public static final class xml {
-        public static final int list_row_background=0x7f040000;
-        public static final int text=0x7f040001;
+        public static final int list_row_background=0x7f050000;
+        public static final int progresspic=0x7f050001;
+        public static final int text=0x7f050002;
     }
 }
