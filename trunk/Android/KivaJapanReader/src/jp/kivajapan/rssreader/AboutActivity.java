57178c71049d42df17dc99125ec3a32936a1b49b
yuji5296@gmail.com <yuji5296@gmail.com@be446a46-2433-11df-8743-957545a88a67>
2011-06-05 10:19:34 +0000

    2011/06/05 Ver.1.4.1 ・トップページ、翻訳一覧の起業家写真のサイズを修正

--- trunk/Android/KivaJapanReader/src/jp/kivajapan/rssreader/AboutActivity.java
+++ trunk/Android/KivaJapanReader/src/jp/kivajapan/rssreader/AboutActivity.java
@@ -1,12 +1,28 @@
 package jp.kivajapan.rssreader;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutActivity extends Activity {	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {		
 		super.onCreate(savedInstanceState);				
 		setContentView(R.layout.about);		
+		
+		//Manifestからバージョン名を取得して表示
+		try {
+		    String packegeName = getPackageName();
+		    PackageInfo packageInfo;
+			packageInfo = getPackageManager().getPackageInfo(packegeName, PackageManager.GET_META_DATA);
+			TextView version = (TextView)findViewById(R.id.Version);
+			version.setText("Version."+ packageInfo.versionName);
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
